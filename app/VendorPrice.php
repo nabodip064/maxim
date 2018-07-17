b1d5279b3a25b67a28d942ff5332a918a4fbf4b6
tapumandal <online.tapu@gmail.com>
2018-07-17 18:33:14 +0600

    Vendor-Company Price is Save Successfully

---
+++ app/VendorPrice.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VendorPrice extends Model
+{
+    protected $table = 'mxp_vendor_prices';
+    protected $primaryKey = 'price_id';
+
+}
